1331672dc4caa9ea19c024ba3a6950d1ef223594
hrAnand1999 <hanand18106024@gmail.com>
2021-03-30 10:57:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+console.log('This is my Script.js');
+const data = [
+    {
+        name: 'Rohan Das',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/51.jpg'
+    },
+
+    {
+        name: 'Shubham Sharma',
+        age: 28,
+        city: 'Bangalore',
+        language: 'JavaScript',
+        framework: 'Angular',
+        image: 'https://randomuser.me/api/portraits/men/54.jpg'
+    },
+
+    {
+        name: 'Camella Cabello',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/women/55.jpg'
+    },
+
+    {
+        name: 'Aishwariya Rai',
+        age: 45,
+        city: 'Mumbai',
+        language: 'Python',
+        framework: 'Flask',
+        image: 'https://randomuser.me/api/portraits/women/57.jpg'
+    },
+
+    {
+        name: 'Rohit Sharma',
+        age: 34,
+        city: 'Jharkhand',
+        language: 'Go',
+        framework: 'Go Framework',
+        image: 'https://randomuser.me/api/portraits/men/61.jpg'
+    }
+]
+
+// CV Iterator
+cvIterator = (profiles) => {
+    let nextIndex = 0;
+    return {
+        next: () => {
+            return nextIndex < profiles.length ?
+                { value: profiles[nextIndex++], done: false } :
+                { done: true }
+        }
+    }
+}
+
+
+const next = document.getElementById('next');
+
+
+
+const candidates = cvIterator(data);
+nextCV = () => {
+    const currentCandidate = candidates.next().value;
+    let image = document.getElementById('image');
+    let profile = document.getElementById('profile');
+    if(currentCandidate != undefined){
+    image.innerHTML = `<img src="${currentCandidate.image}" alt="">`;
+    profile.innerHTML = `<ul class="list-group">
+    <li class="list-group-item">Name: ${currentCandidate.name}</li>
+    <li class="list-group-item">${currentCandidate.age} years old</li>
+    <li class="list-group-item">Lives in ${currentCandidate.city}</li>
+    <li class="list-group-item">Primarily works on ${currentCandidate.language}</li>
+    <li class="list-group-item">Uses ${currentCandidate.framework} framework</li>
+  </ul>`;
+    }
+    else{
+        alert('End of Candidate Application');
+        window.location.reload();
+    }
+}
+nextCV();
+next.addEventListener('click', nextCV);
